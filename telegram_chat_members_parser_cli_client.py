31ad7d6f59f3be0a77a485f18541b7a6dea32f67
Володимир Слободян <opitral@proton.me>
2024-07-05 02:25:24 +0300

    deleted logs folder

--- telegram_chat_members_parser_cli_client.py
+++ telegram_chat_members_parser_cli_client.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("logs/parser.log")
+file_handler = logging.FileHandler("parser.log")
 file_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s - [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
