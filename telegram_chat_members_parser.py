8d44ffc9167279d7c5b10032ed52711c2398a586
Володимир Слободян <opitral@proton.me>
2024-07-04 03:04:10 +0300

    added parsing from messages

--- telegram_chat_members_parser.py
+++ telegram_chat_members_parser.py
@@ -45,6 +45,8 @@ class Parser:
 
         self.from_chats = self.get_chats()
 
+        self.created_at = datetime.now().strftime("%Y-%m-%d %H:%M")
+
     def get_chats(self) -> List[str]:
         try:
             with open(self.txt_path, "r") as f:
@@ -73,56 +75,88 @@ class Parser:
         my_account = await self.bot.get_me()
         lead_id = 1
         members = []
+        chat_in_progress_id = None
 
         for chat_link in self.from_chats:
             try:
-                current_chat_members = []
                 self.logger.info(f"Parsing chat: {chat_link}")
 
                 chat = await self.bot.join_chat(chat_link)
+                chat_in_progress_id = chat.id
                 self.logger.info(f"Joined chat: {chat_link}")
 
-                self.logger.info("Attempting to parse from: list")
+                self.logger.info("Parsing type: list")
                 async for member in self.bot.get_chat_members(chat.id):
-                    if (member.user.is_bot or
-                            member.user.is_deleted or
-                            member.status == ChatMemberStatus.OWNER or
-                            member.status == ChatMemberStatus.ADMINISTRATOR or
-                            member.user.id in tg_lead_ids or
-                            member.user.id == my_account.id):
+                    try:
+                        if (member.user.is_bot or
+                                member.user.is_deleted or
+                                member.status == ChatMemberStatus.OWNER or
+                                member.status == ChatMemberStatus.ADMINISTRATOR or
+                                member.user.id in tg_lead_ids or
+                                member.user.id == my_account.id):
+                            continue
+
+                        lead = {
+                            "id": lead_id,
+                            "telegram_id": member.user.id,
+                            "username": member.user.username,
+                            "first_name": member.user.first_name,
+                            "last_name": member.user.last_name,
+                            "phone_number": member.user.phone_number,
+                            "from_chat": chat_link,
+                            "status": "awaits"
+                        }
+
+                        lead_id += 1
+                        tg_lead_ids.append(lead["telegram_id"])
+                        members.append(lead)
+
+                        self.logger.info(lead)
+
+                    except Exception as ex:
+                        self.logger.error(f"Error parsing member {member}, details: {ex}")
                         continue
 
-                    lead = {
-                        "id": lead_id,
-                        "telegram_id": member.user.id,
-                        "username": member.user.username,
-                        "first_name": member.user.first_name,
-                        "last_name": member.user.last_name,
-                        "phone_number": member.user.phone_number,
-                        "from_chat": chat_link,
-                        "status": "awaits"
-                    }
+                if len(members) == 0:
+                    self.logger.info("Parsing type: messages")
+                    async for message in self.bot.get_chat_history(chat.id):
+                        try:
+                            if len(members) % 100 == 0:
+                                self.save_members(members)
 
-                    lead_id += 1
-                    tg_lead_ids.append(lead["telegram_id"])
-                    current_chat_members.append(lead)
+                            if (message.from_user.is_bot or
+                                    message.from_user.is_deleted or
+                                    message.from_user.id in tg_lead_ids or
+                                    message.from_user.id == my_account.id):
+                                continue
 
-                    self.logger.info(lead)
+                            lead = {
+                                "id": lead_id,
+                                "telegram_id": message.from_user.id,
+                                "username": message.from_user.username,
+                                "first_name": message.from_user.first_name,
+                                "last_name": message.from_user.last_name,
+                                "phone_number": message.from_user.phone_number,
+                                "from_chat": chat_link,
+                                "status": "awaits"
+                            }
 
-                if len(current_chat_members) == 0:
-                    self.logger.info("Attempting to parse from: messages")
-                    # TODO
+                            lead_id += 1
+                            tg_lead_ids.append(lead["telegram_id"])
+                            members.append(lead)
 
-                else:
-                    members.extend(current_chat_members)
+                            self.logger.info(lead)
 
-                self.logger.info(f"Participants received from {chat_link}: {len(current_chat_members)}")
-
-                await self.bot.leave_chat(chat.id)
-                self.logger.info(f"Leaved chat: {chat_link}")
+                        except Exception as ex:
+                            self.logger.error(f"Error parsing message {message}, details: {ex}")
+                            continue
 
             except Exception as ex:
                 self.logger.error(f"Error fetching members from {chat_link}, details: {ex}")
+
+            finally:
+                await self.bot.leave_chat(chat_in_progress_id)
+                self.logger.info(f"Leaved chat: {chat_link}")
 
         return members
 
@@ -133,7 +167,7 @@ class Parser:
             db_name = self.db_name
             members = await self.get_members()
             members_count = len(members)
-            created_at = datetime.now().strftime("%Y-%m-%d %H:%M")
+            created_at = self.created_at
             from_chats = self.from_chats
 
             await self.bot.stop()
@@ -156,13 +190,10 @@ class Parser:
 
         return db_data
 
-    async def create_db(self) -> str:
+    async def create_db(self):
         try:
             db_data = await self.get_db_data()
             db_data_json = json.dumps(db_data, indent=4, ensure_ascii=False)
-
-            if not os.path.exists("results"):
-                os.makedirs("results")
 
             with open(f"results/{self.db_name}.json", "w", encoding="utf-8") as f:
                 f.write(db_data_json)
@@ -176,4 +207,30 @@ class Parser:
         else:
             self.logger.info(f"Database created: {db_path}")
 
-        return db_path
+    def save_members(self, members):
+        try:
+            db_name = self.db_name
+            members_count = len(members)
+            created_at = self.created_at
+            from_chats = self.from_chats
+
+            db_data = {
+                "db_name": db_name,
+                "created_at": created_at,
+                "members_count": members_count,
+                "from_chats": from_chats,
+                "members": members
+            }
+
+            db_data_json = json.dumps(db_data, indent=4, ensure_ascii=False)
+
+            with open(f"results/{self.db_name}.json", "w", encoding="utf-8") as f:
+                f.write(db_data_json)
+
+            db_path = os.getcwd() + "/results/" + self.db_name + ".json"
+
+            self.logger.info(f"Database saved: {db_path}")
+
+        except Exception as ex:
+            self.logger.error(f"Error while saving database, details: {ex}")
+            sys.exit(1)
