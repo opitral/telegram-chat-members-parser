f4e4a3b1aef33a059eddb1095fed975d90cc4a4e
Володимир Слободян <opitral@proton.me>
2024-07-08 16:06:39 +0300

    Return to pyrogram. Added username and phone filters.

--- telegram_chat_members_parser_cli_client.py
+++ telegram_chat_members_parser_cli_client.py
@@ -1,20 +1,16 @@
 import os
 import sys
 import time
-from datetime import datetime, timezone
+from datetime import datetime
 import asyncio
 import logging
 import configparser
 import sqlite3
 
-from pyrogram.errors import FloodWait
-from telethon import TelegramClient
-from telethon.tl import functions
-from telethon.tl.types import UserStatusLastMonth, ChatParticipantCreator
-from telethon.errors.rpcerrorlist import FloodWaitError, UserAlreadyParticipantError
-
 from pyrogram import Client
-from pyrogram.enums import UserStatus
+from pyrogram.enums import ChatMemberStatus, UserStatus
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, FloodWait
+from pyrogram.types import Chat
 
 from typing import List, Dict
 
@@ -26,7 +22,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler("parser.log")
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.WARNING)
 
 formatter = logging.Formatter("%(asctime)s - [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 console_handler.setFormatter(formatter)
@@ -39,16 +35,15 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 session = config["Telegram"]["session"]
-api_id = int(config["Telegram"]["api_id"])
+api_id = config["Telegram"]["api_id"]
 api_hash = config["Telegram"]["api_hash"]
+history_period = config["Parser"]["history_period"]
 
-telethon_client = TelegramClient(f"{session}_telethon", api_id, api_hash)
-pyrogram_client = Client(f"{session}_pyrogram", api_id, api_hash)
-
+bot = Client(session, api_id, api_hash)
 logger.info(f"Session started: {session}")
 
 
-def format_seconds(seconds):
+def format_seconds(seconds: int) -> str:
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
@@ -101,7 +96,6 @@ def create_db(db_name: str, from_chats: List[str]) -> str:
             CREATE TABLE IF NOT EXISTS members (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 telegram_id INTEGER UNIQUE NOT NULL,
-                access_hash VARCHAR(255),
                 username VARCHAR(255),
                 first_name VARCHAR(255),
                 last_name VARCHAR(255),
@@ -114,11 +108,11 @@ def create_db(db_name: str, from_chats: List[str]) -> str:
             )
         ''')
 
-        for from_chat in from_chats:
+        for link in from_chats:
             cursor.execute('''
                 INSERT INTO from_chats (link) 
                 VALUES (?)
-            ''', (from_chat,))
+            ''', (link,))
 
         conn.commit()
         conn.close()
@@ -139,21 +133,11 @@ def update_db(db_name: str, member: Dict):
         cursor = conn.cursor()
 
         cursor.execute('''
-            INSERT INTO members (
-            telegram_id,
-            access_hash,
-            username, 
-            first_name, 
-            last_name, 
-            phone,
-            photo,
-            premium,
-            from_chat_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO members (telegram_id, username, first_name, last_name, phone, photo, premium, from_chat_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''',
                        (
                            member["telegram_id"],
-                           member["access_hash"],
                            member["username"],
                            member["first_name"],
                            member["last_name"],
@@ -183,27 +167,20 @@ def update_db(db_name: str, member: Dict):
         logger.info(f"Database updated with total: {total_members_count}")
 
 
-async def join_chat(chat_link):
+async def join_chat(link: str) -> Chat:
     try:
-        if chat_link.startswith("https://t.me/"):
-            await telethon_client(functions.messages.ImportChatInviteRequest(chat_link.split("/")[-1].replace("+", "")))
+        chat = await bot.join_chat(link)
+        logger.info(f"Joined chat: {link}")
+        return chat
 
-        else:
-            await telethon_client(functions.channels.JoinChannelRequest(chat_link))
+    except UserAlreadyParticipant:
+        logger.info(f"Already participant: {link}")
+        chat = await bot.get_chat(link)
+        return chat
 
-        logger.info(f"Joined chat: {chat_link}")
-        current_chat = await telethon_client.get_entity(chat_link)
-        return current_chat
-
-    except UserAlreadyParticipantError:
-        logger.error(f"Is already a participant of the chat: {chat_link}")
-        current_chat = await telethon_client.get_entity(chat_link)
-        return current_chat
-
-    except FloodWaitError as e:
-        logger.warning(f"Flood wait: {e.seconds} seconds")
-        await asyncio.sleep(e.seconds)
-        await join_chat(chat_link)
+    except FloodWait as e:
+        logger.warning(f"Flood wait: {e.value} seconds")
+        await asyncio.sleep(e.value)
 
 
 async def main():
@@ -214,17 +191,13 @@ async def main():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    await telethon_client.start()
-    await pyrogram_client.start()
-
-    current_chat_telethon = None
-    days_limit = 90
+    await bot.start()
+    current_chat = None
     start_timer = time.time()
 
     try:
-        account = await pyrogram_client.get_me()
-        current_datetime_pyrogram = datetime.now()
-        current_datetime_telethon = datetime.now(timezone.utc)
+        account = await bot.get_me()
+        current_datetime = datetime.now()
 
         cursor.execute("SELECT id, link FROM from_chats")
         from_chats = cursor.fetchall()
@@ -234,80 +207,59 @@ async def main():
             from_chat_link = from_chat[1]
             try:
                 try:
-                    current_chat_telethon = await join_chat(from_chat_link)
-                    current_chat_pyrogram = await pyrogram_client.get_chat(from_chat_link)
+                    current_chat = await join_chat(from_chat_link)
 
                 except Exception as ex:
-                    logger.error(f"Error while joining to group {from_chat_link}, details: {ex}")
+                    logger.info(f"Error while joining to chat {from_chat_link}, details: {ex}")
                     continue
 
                 logger.info("Parsing type: list")
-                async for member in telethon_client.iter_participants(current_chat_telethon):
-                    try:
-                        if (member.bot or
-                                member.deleted or
-                                isinstance(member.status, (UserStatusLastMonth,)) or
-                                isinstance(member.status, (ChatParticipantCreator,)) or
-                                member.id == account.id):
+                try:
+                    async for member in bot.get_chat_members(current_chat.id):
+                        if (not (member.user.username or member.user.phone_number) or
+                                member.user.is_bot or
+                                member.user.is_deleted or
+                                member.user.status == UserStatus.LONG_AGO or
+                                member.status == ChatMemberStatus.OWNER or
+                                member.user.id == account.id):
                             continue
 
-                        cursor.execute("SELECT * FROM members WHERE telegram_id = ?", (member.id,))
+                        cursor.execute("SELECT * FROM members WHERE telegram_id = ?", (member.user.id,))
                         found_lead = cursor.fetchone()
+
                         if found_lead:
                             continue
 
-                        access_hash = None
-                        if not (member.username or member.phone):
-                            lead_entity = await telethon_client.get_entity(member.id)
-                            access_hash = lead_entity.access_hash
-
                         lead = {
-                            "telegram_id": member.id,
-                            "access_hash": access_hash,
-                            "username": member.username,
-                            "first_name": member.first_name,
-                            "last_name": member.last_name,
-                            "phone": member.phone,
-                            "premium": member.premium,
-                            "photo": member.photo is not None,
-                            "from_chat_id": from_chat_id
+                            "telegram_id": member.user.id,
+                            "username": member.user.username,
+                            "first_name": member.user.first_name,
+                            "last_name": member.user.last_name,
+                            "phone": member.user.phone_number,
+                            "photo": member.user.photo is not None,
+                            "premium": member.user.is_premium,
+                            "target_chat_id": from_chat_id
                         }
 
                         update_db(db_name, lead)
 
-                    except FloodWaitError as e:
-                        logger.warning(f"Flood wait: {e.seconds} seconds")
-                        await asyncio.sleep(e.seconds)
+                except FloodWait as e:
+                    logger.warning(f"Flood wait: {e.value} seconds")
+                    await asyncio.sleep(e.value)
 
-                    except Exception as ex:
-                        logger.error(f"Error parsing list {member}, details: {ex}")
-                        continue
+                except Exception as ex:
+                    logger.error(f"Error parsing list, details: {ex}")
+                    continue
 
                 logger.info("Parsing type: messages")
-                logger.info("Contact with participants")
-                day = 0
-                async for message in telethon_client.iter_messages(current_chat_telethon):
-                    try:
-                        date_difference = current_datetime_telethon - message.date
-
-                        if date_difference.days > day:
-                            logger.info(f"Day: {date_difference.days}")
-                            day = date_difference.days
-
-                        if date_difference.days >= days_limit:
-                            break
-
-                    except FloodWaitError as e:
-                        logger.warning(f"Flood wait: {e.seconds} seconds")
-                        await asyncio.sleep(e.seconds)
-
-                async for message in pyrogram_client.get_chat_history(current_chat_pyrogram.id):
-                    try:
-                        date_difference = current_datetime_pyrogram - message.date
-                        if date_difference.days >= days_limit:
+                try:
+                    async for message in bot.get_chat_history(current_chat.id):
+                        date_difference = current_datetime - message.date
+                        if date_difference.days >= 90:
                             break
 
                         if (message.sender_chat or
+                                not (message.from_user.username or message.from_user.phone_number) or
                                 message.from_user.is_bot or
                                 message.from_user.is_deleted or
                                 message.from_user.status == UserStatus.LONG_AGO or
@@ -320,48 +272,30 @@ async def main():
                         if found_lead:
                             continue
 
-                        access_hash = None
-                        if not (message.from_user.username or message.from_user.phone_number):
-                            lead_entity = await telethon_client.get_entity(message.from_user.id)
-                            access_hash = lead_entity.access_hash
-
                         lead = {
                             "telegram_id": message.from_user.id,
-                            "access_hash": access_hash,
                             "username": message.from_user.username,
                             "first_name": message.from_user.first_name,
                             "last_name": message.from_user.last_name,
                             "phone": message.from_user.phone_number,
-                            "premium": message.from_user.is_premium,
                             "photo": message.from_user.photo is not None,
+                            "premium": message.from_user.is_premium,
                             "from_chat_id": from_chat_id
                         }
 
                         update_db(db_name, lead)
 
-                    except FloodWaitError as e:
-                        logger.warning(f"Flood wait: {e.seconds} seconds")
-                        await asyncio.sleep(e.seconds)
+                except FloodWait as e:
+                    logger.warning(f"Flood wait: {e.value} seconds")
+                    await asyncio.sleep(e.value)
 
-                    except FloodWait as e:
-                        logger.warning(f"Flood wait: {e.value} seconds")
-                        await asyncio.sleep(e.value)
-
-                    except Exception as ex:
-                        logger.error(f"Error parsing messages {message}, details: {ex}")
-                        continue
+                except Exception as ex:
+                    logger.error(f"Error parsing messages, details: {ex}")
+                    continue
 
                 cursor.execute("SELECT members FROM from_chats WHERE id = ?", (from_chat_id,))
                 current_chat_members_count = cursor.fetchone()[0]
                 logger.info(f"From {from_chat_link} received members: {current_chat_members_count}")
-
-            except FloodWaitError as e:
-                logger.warning(f"Flood wait: {e.seconds} seconds")
-                await asyncio.sleep(e.seconds)
-
-            except FloodWait as e:
-                logger.warning(f"Flood wait: {e.value} seconds")
-                await asyncio.sleep(e.value)
 
             except Exception as ex:
                 logger.error(f"Error parsing members, details: {ex}")
@@ -369,11 +303,13 @@ async def main():
 
             finally:
                 try:
-                    await telethon_client(functions.channels.LeaveChannelRequest(current_chat_telethon.id))
+                    await bot.leave_chat(current_chat.id)
+
+                except UserNotParticipant:
+                    logger.warning(f"Was banned in: {from_chat_link}")
 
                 except Exception as ex:
-                    logger.error(ex)
-                    logger.warning(f"Account was banned in: {from_chat_link}")
+                    logger.error(f"Error while leaving from {from_chat_link}, details: {ex}")
 
                 else:
                     logger.info(f"Leaved chat: {from_chat_link}")
@@ -382,11 +318,10 @@ async def main():
         logger.error(ex)
 
     finally:
-        total_timer = time.time() - start_timer
-        logger.info(f"Script worked: {format_seconds(total_timer)}")
         conn.close()
-        await telethon_client.disconnect()
-        await pyrogram_client.stop()
+        await bot.stop()
+        total_timer = int(time.time() - start_timer)
+        logger.info(f"Script worked: {format_seconds(total_timer)}")
 
 
 if __name__ == "__main__":
